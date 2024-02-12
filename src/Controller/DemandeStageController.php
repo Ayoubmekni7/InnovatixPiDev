22517a511a4b8b1a7ffbec1bbe2acde803b43b76
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2024-02-12 19:20:40 +0100

    création de base de données + create Demande.

--- src/Controller/DemandeStageController.php
+++ src/Controller/DemandeStageController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Demandestage;
+use App\Form\DemandeStageType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DemandeStageController extends AbstractController
 {
-    #[Route('/demande/stage', name: 'app_demande_stage')]
-    public function index(): Response
+    #[Route('/demandeStage', name: 'demandeStage')]
+    public function demandeStage(Request $request,ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('demande_stage/index.html.twig', [
-            'controller_name' => 'DemandeStageController',
+        $demande = new Demandestage();
+        $form = $this->createForm(DemandeStageType::class, $demande);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $x = $managerRegistry->getManager();
+            $x->persist($demande);
+            $x->flush();
+        }
+        return $this->render('demande_stage/demande.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
