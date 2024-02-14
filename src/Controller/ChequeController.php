2ac51fbe2563d7cbcd2708f71deb8c7efeb3cb1e
shaymaouerhani <shayma.ouerhani@esprit.tn>
2024-02-14 22:49:51 +0100

    creation crud cheque et creation de base de donne

--- src/Controller/ChequeController.php
+++ src/Controller/ChequeController.php
@@ -2,17 +2,79 @@
 
 namespace App\Controller;
 
+use App\Entity\Cheque;
+use App\Form\ChequeType;
+use App\Repository\ChequeRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChequeController extends AbstractController
 {
-    #[Route('/cheques', name: 'app_cheque')]
-    public function index(): Response
+    #[Route('/addcheques', name: 'addcheques')]
+    public function addcheques(ChequeRepository $chequeRepository, Request $request, ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('cheque/list.html.twig', [
-            'controller_name' => 'ChequeController',
+        $cheque = new Cheque();
+        $form = $this->createForm(ChequeType::class, $cheque);
+        $em = $managerRegistry->getManager();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($cheque);
+            $em->flush();
+            return new Response("shayma");
+        }
+
+        return $this->render('cheque/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
+
+    #[Route('/AfficherDemande', name: 'AfficherDemande')]
+     public function AfficherDemande(ChequeRepository $chequeRepository):Response
+
+    {
+        $liste= $chequeRepository->findAll();
+        return $this->render('cheque/list.html.twig',[
+            'cheques'=>$liste,
+        ]);
+    }
+    #[Route('/deleteDemandeCheque/{id}', name: 'deleteDemandeCheque')]
+     public function deleteDemandeCheque($id,ManagerRegistry $managerRegistry,ChequeRepository $repository):Response
+    {
+        $emm=$managerRegistry->getManager();
+        $idremove=$repository->find($id);
+        $emm->remove($idremove);
+        $emm->flush();
+        return $this->redirectToRoute('AfficherDemande');
+
+    }
+#[Route('/modifierCheque/{id}', name: 'modifierCheque')]
+public function modifierCheque($id,ManagerRegistry $managerRegistry,ChequeRepository $chequeRepository , Request $request):Response
+{
+    $em=$managerRegistry->getManager();
+    $idData =$chequeRepository->find($id);
+    $form=$this->createForm(ChequeType::class,$idData);
+    $form->handleRequest($request);
+    if($form->isSubmitted() and $form->isValid()){
+        $em->persist($idData);
+        $em->flush();
+        return new Response("modification avec succes");
+
+    }
+    return $this->renderForm('cheque/add.html.twig',[
+        'form' => $form
+    ]);
+}
+
+
+
+
+
+
+
+
 }
