54d9f0ceca33c92e28c5661d9dda27ef6cd46d7c
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2024-02-15 15:40:07 +0100

    crud de Contrat sans design

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Contrat;
+use App\Form\ContratType;
+use App\Form\StageType;
+use App\Repository\ContratRepository;
+use App\Repository\StageRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ContratController extends AbstractController
 {
@@ -15,6 +23,57 @@ class ContratController extends AbstractController
             'controller_name' => 'ContratController',
         ]);
     }
+    #[Route('/addContrat', name: 'addContrat')]
+    public function addContrat(Request $request,ManagerRegistry $managerRegistry,SluggerInterface $slugger): Response
+    {
+        $stage = new Contrat();
+        $form = $this->createForm(ContratType::class, $stage);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $x = $managerRegistry->getManager();
+            $x->persist($stage);
+            $x->flush();
+        }
+        return $this->render('contrat/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
     
     
+    
+    #[Route('/AffichageDesContrats', name: 'AffichageDesContrats')]
+    public function AffichageDesContrats(ContratRepository $contratRepository): Response
+    {
+        $liste = $contratRepository->findAll();
+        return $this->render('demande_stage/affichage.html.twig', [
+            'contrat' => $liste,
+        ]);
+    }
+    
+    #[Route('/deleteContrat/{id}', name: 'deleteContrat')]
+    public function deleteContrat($id, ManagerRegistry $manager, ContratRepository $contratRepository): Response
+    {
+        $emm = $manager->getManager();
+        $idremove = $contratRepository->find($id);
+        $emm->remove($idremove);
+        $emm->flush();
+        return $this->redirectToRoute('demandeStage');
+    }
+    
+    #[Route('/modifierContrat/{id}', name: 'modifierContrat')]
+    public function modifierContrat($id, ManagerRegistry $manager, StageRepository $stageRepository, Request $request,): Response
+    {
+        $emr = $manager->getManager();
+        $idData = $stageRepository->find($id);
+        $form = $this->createForm(ContratType::class, $idData);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $emr->persist($idData);
+            $emr->flush();
+            return new Response("update with succcess");
+        }
+        return $this->renderForm('contrat/demande.html.twig', [
+            'form' => $form
+        ]);
+    }
 }
