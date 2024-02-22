e36867c1b3db741cbd4f971f6d041f3c08c6f6c3
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-02-22 03:09:02 +0100

    update

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -11,7 +11,28 @@ class ActualiteController extends AbstractController
     #[Route('/actualite', name: 'app_actualite')]
     public function index(): Response
     {
-        return $this->render('actualite/index.html.twig', [
+        return $this->render('front/listArticles.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+    #[Route('/client', name: 'client')]
+    public function client(): Response
+    {
+        return $this->render('employe/reclamationEmploye.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+    #[Route('/clientall', name: 'clientall')]
+    public function clientall(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'ActualiteController',
+        ]);
+    }
+    #[Route('/afficherRecRep', name: 'app_client')]
+    public function afficherRecRep(): Response
+    {
+        return $this->render('client.html.twig', [
             'controller_name' => 'ActualiteController',
         ]);
     }
