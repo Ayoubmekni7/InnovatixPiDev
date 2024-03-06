570aafbac08e657f76a1f8311e24e5ab9a95bb00
ayoub mekni <ayoubmekni211@gmail.com>
2024-03-06 15:03:34 +0100

    ayoub

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,7 +1,15 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Evenement;
+use App\Form\EvenementType;
+use App\Repository\EvenementRepository;
+use App\Entity\Investissement;
+use App\Form\InvestissementType;
+use App\Repository\InvestissementRepository;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +20,32 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/BaseFront.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+
+    #[Route('/upload.php', name: 'upload')]
+    public function upload(): Response
+    {
+        return $this->render('main/upload.php', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/event', name: 'app_event', methods: ['GET'])]
+    public function event(CommentaireRepository $commentaireRepository, InvestissementRepository $investissementRepository, EvenementRepository $evenementRepository): Response
+    {
+        return $this->render('main/eventsweb.html.twig', [
+            'commentaires' => $commentaireRepository->findAll(),
+            'investissements' => $investissementRepository->findAll(),
+            'evenements' => $evenementRepository->findAll(),
+        ]);
+    }
+    #[Route('/client/dashboard', name: 'app_dashboard')]
+    public function index1(): Response
+    {
+        return $this->render('client/dashboard.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
